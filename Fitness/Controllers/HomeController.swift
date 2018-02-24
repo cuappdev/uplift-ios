39ceaba6243ed7ff25cc18dab8ac8c08c3c191a0
Keivan Shahida <keivanshahida@icloud.com>
2018-02-24 16:10:37 -0500

    Initial home and tab bar set up complete.

---
+++ Fitness/Controllers/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeController.swift
+//  Fitness
+//
+//  Created by Keivan Shahida on 2/24/18.
+//  Copyright © 2018 Keivan Shahida. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        
+    }
+}
