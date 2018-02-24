5dc69a13b946014b88af175eade0f96648d4859b
Keivan Shahida <keivanshahida@icloud.com>
2018-02-24 15:55:13 -0500

    Project set up.

---
+++ Fitness/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Fitness
+//
+//  Created by Keivan Shahida on 2/24/18.
+//  Copyright © 2018 Keivan Shahida. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
