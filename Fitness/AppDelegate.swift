45e706e6baa0875727002b0963f935fe88ea243f
Joseph Fulgieri <jmf373@cornell.edu>
2018-02-25 12:12:12 -0500

    fabric setup complete

--- Fitness/AppDelegate.swift
+++ Fitness/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,12 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = TabBarController()
         
         
+        Fabric.with([Crashlytics.self])
         return true
     }
 }
