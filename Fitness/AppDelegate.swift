0131aed18bcbf0ec413d9f1f022332b6b7d87195
Jack Schluger <jschluger@stuy.edu>
2018-03-05 23:05:05 -0500

    starting work on ClassDetailView. Added the scrollView, title, subtitle, image, and description textView. Fonts for text not working.

--- Fitness/AppDelegate.swift
+++ Fitness/AppDelegate.swift
@@ -23,7 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         window?.rootViewController = TabBarController()
         
-        
         Fabric.with([Crashlytics.self])
         return true
     }
