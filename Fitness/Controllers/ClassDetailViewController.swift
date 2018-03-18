6caf8347d3d9eeceaef2d837e179cbfa4fb933c3
Keivan Shahida <keivanshahida@icloud.com>
2018-03-18 13:30:19 -0400

    Updated detail view designs

--- Fitness/Controllers/ClassDetailViewController.swift
+++ Fitness/Controllers/ClassDetailViewController.swift
@@ -13,12 +13,17 @@ class ClassDetailViewController: UIViewController {
     
     var titleLabel: UILabel!
     var locationLabel: UILabel!
-    
+    var instructorLabel: UILabel!
+    var durationLabel: UILabel!
+
     var classImageView: UIImageView!
     var imageFilterView: UIView!
+    var semicircleView: UIImageView!
+    
+    var backButton: UIButton!
+    var starButton: UIButton!
 
-    var monthLabel: UILabel!
-    var dayLabel: UILabel!
+    var dateLabel: UILabel!
     var timeLabel: UILabel!
     
     var addToCalendarButton: UIButton!
@@ -45,7 +50,8 @@ class ClassDetailViewController: UIViewController {
         scrollView = UIScrollView()
         scrollView.showsVerticalScrollIndicator = false
         scrollView.isScrollEnabled = true
-        scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height * 2)
+        scrollView.bounces = false
+        scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height * 1.9)
         view.addSubview(scrollView)
         scrollView.snp.makeConstraints { (make) in
             make.edges.equalToSuperview()
@@ -54,55 +60,79 @@ class ClassDetailViewController: UIViewController {
         contentView = UIView()
         scrollView.addSubview(contentView)
         contentView.snp.makeConstraints { (make) in
-            make.top.equalTo(scrollView)
+            make.top.equalToSuperview()
             make.left.right.equalTo(view)
         }
         
         //HEADER
         classImageView = UIImageView(image: #imageLiteral(resourceName: "running-sample"))
         classImageView.contentMode = UIViewContentMode.scaleAspectFill
+        classImageView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(classImageView)
         
         imageFilterView = UIView()
-        imageFilterView.backgroundColor = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 0.5)
+        imageFilterView.backgroundColor = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 0.6)
         contentView.addSubview(imageFilterView)
         
+        semicircleView = UIImageView(image: #imageLiteral(resourceName: "semicircle"))
+        semicircleView.contentMode = UIViewContentMode.scaleAspectFit
+        semicircleView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(semicircleView)
+        
         titleLabel = UILabel()
-        titleLabel.text = "Urban H.I.I.T"
-        titleLabel.font = UIFont(name: "Futura-HeavyOblique", size: 48)
+        titleLabel.text = "MUSCLE PUMP"
+        titleLabel.font = ._48Bebas
         titleLabel.textAlignment = .center
         titleLabel.textColor = .white
         titleLabel.sizeToFit()
         contentView.addSubview(titleLabel)
         
         locationLabel = UILabel()
-        locationLabel.text = "HELEN NEWMAN HALL"
-        locationLabel.font = ._16SFLight
+        locationLabel.text = "Helen Newman Hall Dance Studio"
+        locationLabel.font = ._14MontserratLight
         locationLabel.textAlignment = .center
         locationLabel.textColor = .white
         locationLabel.sizeToFit()
         contentView.addSubview(locationLabel)
         
-        //DATE
-        monthLabel = UILabel()
-        monthLabel.text = "FEBRUARY 28"
-        monthLabel.font = ._18SFBold
-        monthLabel.textAlignment = .center
-        monthLabel.textColor = .fitnessBlack
-        monthLabel.sizeToFit()
-        contentView.addSubview(monthLabel)
+        instructorLabel = UILabel()
+        instructorLabel.text = "DEBBIE"
+        instructorLabel.font = ._18Bebas
+        instructorLabel.textAlignment = .center
+        instructorLabel.textColor = .white
+        instructorLabel.sizeToFit()
+        contentView.addSubview(instructorLabel)
         
-        dayLabel = UILabel()
-        dayLabel.text = "Wednesday"
-        dayLabel.font = ._14SFLight
-        dayLabel.textAlignment = .center
-        dayLabel.textColor = .fitnessBlack
-        dayLabel.sizeToFit()
-        contentView.addSubview(dayLabel)
+        durationLabel = UILabel()
+        durationLabel.text = "45 min"
+        durationLabel.font = ._18Bebas
+        durationLabel.textAlignment = .center
+        durationLabel.textColor = .fitnessBlack
+        durationLabel.sizeToFit()
+        contentView.addSubview(durationLabel)
+        
+        backButton = UIButton()
+        backButton.setImage(#imageLiteral(resourceName: "back-arrow"), for: .normal)
+        backButton.sizeToFit()
+        contentView.addSubview(backButton)
+        
+        starButton = UIButton()
+        starButton.setImage(#imageLiteral(resourceName: "star"), for: .normal)
+        starButton.sizeToFit()
+        contentView.addSubview(starButton)
+        
+        //DATE
+        dateLabel = UILabel()
+        dateLabel.text = "Wednesday, March 15"
+        dateLabel.font = ._16MontserratLight
+        dateLabel.textAlignment = .center
+        dateLabel.textColor = .fitnessBlack
+        dateLabel.sizeToFit()
+        contentView.addSubview(dateLabel)
         
         timeLabel = UILabel()
-        timeLabel.text = "10:30 AM - 11:30 AM"
-        timeLabel.font = ._14SFRegular
+        timeLabel.text = "12:15PM - 1:00PM"
+        timeLabel.font = ._16MontserratMedium
         timeLabel.textAlignment = .center
         timeLabel.textColor = .fitnessBlack
         timeLabel.sizeToFit()
@@ -129,15 +159,15 @@ class ClassDetailViewController: UIViewController {
         //FUNCTION
         functionLabel = UILabel()
         functionLabel.text = "FUNCTION"
-        functionLabel.font = ._16SFBold
+        functionLabel.font = ._16MontserratMedium
         functionLabel.textAlignment = .center
         functionLabel.textColor = .fitnessBlack
         functionLabel.sizeToFit()
         contentView.addSubview(functionLabel)
         
         functionDescriptionLabel = UILabel(frame: CGRect(x: 10, y: 511, width: 100, height: 19))
-        functionDescriptionLabel.text = "Strength, Core, Cardio"
-        functionDescriptionLabel.font = ._14SFRegular
+        functionDescriptionLabel.text = "Core  · Overall Fitness · Stability"
+        functionDescriptionLabel.font = ._14MontserratLight
         functionDescriptionLabel.textAlignment = .center
         functionDescriptionLabel.textColor = .fitnessBlack
         functionDescriptionLabel.sizeToFit()
@@ -149,8 +179,8 @@ class ClassDetailViewController: UIViewController {
         
         //DESCRIPTION
         descriptionTextView = UITextView()
-        descriptionTextView.text = "HIGH INTENSITY INTERVAL training (HIIT) has the fitness industry buzzing because of its potential to torch maximum calories in a minimum amount of time. The idea is that you do short periods of all-out work followed by short periods of active rest to make the body work harder. HIGH INTENSITY INTERVAL training (HIIT) has the fitness industry buzzing because of its potential to torch maximum calories in a minimum amount of time. The idea is that you do short periods of all-out work followed by short periods of active rest to make the body work harder. HIGH INTENSITY INTERVAL training (HIIT) has the fitness industry buzzing because of its potential to torch maximum calories in a minimum amount of time. The idea is that you do short periods of all-out work followed by short periods of active rest to make the body work harder. HIGH INTENSITY INTERVAL training (HIIT) has the fitness industry buzzing because of its potential to torch maximum calories in a minimum amount of time. The idea is that you do short periods of all-out work followed by short periods of active rest to make the body work harder. HIGH INTENSITY INTERVAL training (HIIT) has the fitness industry buzzing because of its potential to torch maximum calories in a minimum amount of time. The idea is that you do short periods of all-out work followed by short periods of active rest to make the body work harder. "
-        descriptionTextView.font = ._14SFRegular
+        descriptionTextView.text = "Put a little muscle into your workout and join us for a class designed to build muscle endurance with low to medium weights and high repetitions. A variety of equipment and strength training techniques will be used in this class. There is no cardio portion in these sessions. Footwear that is appropriate for movement is required for this class. "
+        descriptionTextView.font = ._14MontserratLight
         descriptionTextView.isEditable = false
         descriptionTextView.textAlignment = .center
         descriptionTextView.sizeToFit()
@@ -167,49 +197,77 @@ class ClassDetailViewController: UIViewController {
         //HEADER
         classImageView.snp.makeConstraints { make in
             make.left.equalToSuperview()
-            make.top.equalToSuperview()
+            make.top.equalToSuperview().offset(-30)
             make.right.equalToSuperview()
-            make.height.equalTo(320)
+            make.height.equalTo(360)
         }
         
         imageFilterView.snp.makeConstraints { make in
             make.edges.equalTo(classImageView)
         }
         
+        semicircleView.snp.makeConstraints { make in
+            make.bottom.equalTo(classImageView.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+            make.width.equalTo(100)
+        }
+        
         titleLabel.snp.makeConstraints { make in
             make.left.equalToSuperview()
             make.top.equalToSuperview().offset(126)
             make.right.equalToSuperview()
-            make.height.equalTo(58)
+            make.height.equalTo(57)
         }
         
         locationLabel.snp.makeConstraints { make in
                 make.left.equalToSuperview()
                 make.top.equalTo(titleLabel.snp.bottom)
                 make.right.equalToSuperview()
-                make.height.equalTo(19)
+                make.height.equalTo(16)
         }
         
-        //DATE
-        monthLabel.snp.makeConstraints { make in
+        instructorLabel.snp.makeConstraints { make in
             make.left.equalToSuperview()
-            make.top.equalTo(classImageView.snp.bottom).offset(22)
+            make.top.equalTo(locationLabel.snp.bottom).offset(20)
             make.right.equalToSuperview()
             make.height.equalTo(21)
         }
         
-        dayLabel.snp.makeConstraints { make in
+        durationLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(classImageView.snp.bottom).offset(-5)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(50)
+            make.height.equalTo(21)
+        }
+        
+        backButton.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(20)
+            make.top.equalToSuperview().offset(16)
+            make.width.equalTo(23)
+            make.height.equalTo(19)
+        }
+        
+        starButton.snp.makeConstraints { make in
+            make.right.equalToSuperview().offset(-21)
+            make.top.equalToSuperview().offset(14)
+            make.width.equalTo(23)
+            make.height.equalTo(22)
+        }
+        
+        //DATE
+        dateLabel.snp.makeConstraints { make in
             make.left.equalToSuperview()
-            make.top.equalTo(monthLabel.snp.bottom)
+            make.top.equalTo(classImageView.snp.bottom).offset(40)
             make.right.equalToSuperview()
-            make.height.equalTo(16)
+            make.height.equalTo(19)
         }
         
         timeLabel.snp.makeConstraints { make in
             make.left.equalToSuperview()
-            make.top.equalTo(dayLabel.snp.bottom)
+            make.top.equalTo(dateLabel.snp.bottom).offset(8)
             make.right.equalToSuperview()
-            make.height.equalTo(16)
+            make.height.equalTo(19)
         }
         
         //CALENDAR
@@ -217,7 +275,7 @@ class ClassDetailViewController: UIViewController {
             make.height.equalTo(23)
             make.width.equalTo(23)
             make.centerX.equalToSuperview()
-            make.top.equalTo(timeLabel.snp.bottom).offset(17)
+            make.top.equalTo(timeLabel.snp.bottom).offset(26)
         }
         
         addToCalendarLabel.snp.makeConstraints { make in
@@ -229,9 +287,9 @@ class ClassDetailViewController: UIViewController {
         
         dateDivider.snp.makeConstraints { make in
             make.height.equalTo(1)
-            make.left.equalToSuperview().offset(30)
-            make.right.equalToSuperview().offset(-30)
-            make.top.equalTo(addToCalendarLabel.snp.bottom).offset(15)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalTo(addToCalendarLabel.snp.bottom).offset(36)
         }
         
         //FUNCTION
@@ -244,24 +302,23 @@ class ClassDetailViewController: UIViewController {
         
         functionDescriptionLabel.snp.makeConstraints { make in
             make.left.equalToSuperview()
-            make.top.equalTo(functionLabel.snp.bottom).offset(7)
+            make.top.equalTo(functionLabel.snp.bottom).offset(12)
             make.right.equalToSuperview()
-            make.height.equalTo(16)
+            make.height.equalTo(20)
         }
         
         functionDivider.snp.makeConstraints { make in
             make.height.equalTo(1)
-            make.left.equalToSuperview().offset(30)
-            make.right.equalToSuperview().offset(-30)
-            make.top.equalTo(functionDescriptionLabel.snp.bottom).offset(20)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalTo(functionDescriptionLabel.snp.bottom).offset(32)
         }
         
         //DESCRIPTION
-        
         descriptionTextView.snp.makeConstraints { make in
-            make.left.equalToSuperview().offset(48)
-            make.right.equalToSuperview().offset(-48)
-            make.top.equalTo(functionDivider.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(40)
+            make.right.equalToSuperview().offset(-40)
+            make.top.equalTo(functionDivider.snp.bottom).offset(36)
         }
     }
 }
--- Fitness/Extensions/UIColor+Shared.swift
+++ Fitness/Extensions/UIColor+Shared.swift
@@ -11,10 +11,9 @@ import UIKit
 extension UIColor {
 
     @nonobjc static let fitnessWhite = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
-    @nonobjc static let fitnessBlack = UIColor(red: 43/255, green: 43/255, blue: 43/255, alpha: 1.0)
+    @nonobjc static let fitnessBlack = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 1.0)
     
     @nonobjc static let fitnessLightGrey = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 1.0)
-
 
     public static func colorFromCode(_ code: Int) -> UIColor {
         let red = CGFloat(((code & 0xFF0000) >> 16)) / 255
