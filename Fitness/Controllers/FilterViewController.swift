162df277f06f2fcb635acc1ab7d8209575c57904
Joseph Fulgieri <jmf373@cornell.edu>
2018-04-02 22:20:55 -0400

    setup collectionview, slider section views

--- Fitness/Controllers/FilterViewController.swift
+++ Fitness/Controllers/FilterViewController.swift
@@ -17,6 +17,12 @@ class FilterViewController: UIViewController, UICollectionViewDelegateFlowLayout
     
     var collectionViewTitle: UILabel!
     var collectionView: UICollectionView!
+    
+    var separatorOne: UIView!
+    var startTimeLabel: UILabel!
+    var startTime: UILabel!
+    var startTimeSlider: UISlider!  //placeholder: to be replaced with a slider with range functionality
+    var separatorTwo: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,31 +67,103 @@ class FilterViewController: UIViewController, UICollectionViewDelegateFlowLayout
         
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16 )
-        layout.minimumInteritemSpacing = 12
-        layout.minimumLineSpacing = 16
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0 )
+        layout.minimumInteritemSpacing = 3
+        layout.minimumLineSpacing = 0
         
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.backgroundColor = .fitnessLightGrey
+        
+        collectionView.isScrollEnabled = true
+        collectionView.dragInteractionEnabled = true
+        collectionView.alwaysBounceHorizontal = true
+        collectionView.alwaysBounceVertical = true
+        collectionView.bounces = true
+        
         collectionView.register(GymFilterCell.self , forCellWithReuseIdentifier: "gymFilterCell")
         contentView.addSubview(collectionView)
-        collectionView.backgroundColor = .yellow
+        
+        //START TIME SLIDER SECTION
+        separatorOne = UIView()
+        separatorOne.backgroundColor = .fitnessLightGrey
+        contentView.addSubview(separatorOne)
+        
+        startTimeLabel = UILabel()
+        startTimeLabel.sizeToFit()
+        startTimeLabel.font = ._12LatoBlack
+        startTimeLabel.textColor = .fitnessDarkGrey
+        startTimeLabel.text = "START TIME"
+        contentView.addSubview(startTimeLabel)
+        
+        startTime = UILabel()
+        startTime.sizeToFit()
+        startTime.font = ._12LatoBlack
+        startTime.textColor = .fitnessDarkGrey
+        startTime.text = "6:00 AM - 10:00 PM"
+        contentView.addSubview(startTime)
+        
+        startTimeSlider = UISlider()
+        startTimeSlider.maximumTrackTintColor = .fitnessDarkGrey
+        startTimeSlider.minimumTrackTintColor = .fitnessYellow
+        contentView.addSubview(startTimeSlider)
+        
+        separatorTwo = UIView()
+        separatorTwo.backgroundColor = .fitnessLightGrey
+        contentView.addSubview(separatorTwo)
         
         setupConstraints()
     }
     
+    // MARK: - CONSTRAINTS
     func setupConstraints() {
+        //COLLECTION VIEW SECTION
         collectionViewTitle.snp.updateConstraints{make in
             make.left.equalToSuperview().offset(16)
             make.top.equalToSuperview().offset(20)
+            make.bottom.equalTo(collectionViewTitle.snp.top).offset(15)
         }
         
         collectionView.snp.updateConstraints{make in
             make.left.equalToSuperview()
             make.right.equalToSuperview()
             make.top.equalToSuperview().offset(51)
-            make.bottom.equalTo(collectionViewTitle.snp.bottom).offset(60)
+            make.bottom.equalTo(collectionViewTitle.snp.bottom).offset(44)
+        }
+        
+        //SLIDER SECTION
+        separatorOne.snp.updateConstraints{make in
+            make.top.equalTo(collectionView.snp.bottom).offset(16)
+            make.bottom.equalTo(collectionView.snp.bottom).offset(17)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        startTimeLabel.snp.updateConstraints{make in
+            make.left.equalToSuperview().offset(16)
+            make.top.equalTo(separatorOne.snp.bottom).offset(20)
+            make.bottom.equalTo(separatorOne.snp.bottom).offset(35)
+        }
+        
+        startTime.snp.updateConstraints{make in
+            make.right.equalToSuperview().offset(-22)
+            make.top.equalTo(separatorOne.snp.bottom).offset(20)
+            make.bottom.equalTo(separatorOne.snp.bottom).offset(36)
+        }
+        
+        startTimeSlider.snp.updateConstraints{make in
+            make.right.equalToSuperview().offset(-16)
+            make.left.equalToSuperview().offset(16)
+            make.top.equalTo(separatorOne.snp.bottom).offset(47)
+            make.bottom.equalTo(separatorOne.snp.bottom).offset(71)
+        }
+        
+        separatorTwo.snp.updateConstraints{make in
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview()
+            make.top.equalTo(separatorOne.snp.bottom).offset(90)
+            make.bottom.equalTo(separatorOne.snp.bottom).offset(91)
         }
     }
     
@@ -101,7 +179,41 @@ class FilterViewController: UIViewController, UICollectionViewDelegateFlowLayout
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "gymFilterCell", for: indexPath) as! GymFilterCell
         
+        switch indexPath.row {
+        case 0:
+            cell.gymName.text = "Teagle"
+        case 1:
+            cell.gymName.text = "Appel"
+        case 2:
+            cell.gymName.text = "Helen Newman"
+        case 3:
+            cell.gymName.text = "Noyes"
+        default:
+            cell.gymName.text = "Gates"
+        }
+        
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var offset = CGFloat(0)
+        if (view.frame.width > 360){
+            offset = (view.frame.width - 360)/4
+        }
+        
+        switch indexPath.row {
+        case 0:
+            return CGSize(width: 74 + offset, height: 28)
+        case 1:
+            return CGSize(width: 70 + offset, height: 28)
+        case 2:
+            return CGSize(width: 138 + offset, height: 28)
+        case 3:
+            return CGSize(width: 72 + offset, height: 28)
+        default:
+            return CGSize(width: 150 + offset, height: 28)
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
