1b45686db3f9c4527f08db02fed9d78b4357ca7a
Joseph Fulgieri <jmf373@cornell.edu>
2018-04-02 22:20:55 -0400

    setup collectionview, slider section views

--- Fitness/Views/Cells/GymFilterCell.swift
+++ Fitness/Views/Cells/GymFilterCell.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import SnapKit
 
 class GymFilterCell: UICollectionViewCell {
     
+    // MARK: - INITIALIZATION
+    var gymName: UILabel!
+    //var separator: UIView!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .white
         
-        backgroundColor = .blue
+        gymName = UILabel()
+        gymName.font = ._14MontserratLight
+        gymName.textColor = .fitnessBlack
+        gymName.sizeToFit()
+        gymName.textAlignment = .center
+        contentView.addSubview(gymName)
+        
+        /*separator = UIView()
+        separator.backgroundColor = .fitnessLightGrey
+        contentView.addSubview(separator)*/
+        
+        setupConstraints()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    
+    // MARK: - CONSTRAINTS
+    func setupConstraints() {
+        gymName.snp.updateConstraints{make in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().offset(14)
+            make.right.equalToSuperview().offset(-14)
+        }
+        
+        /*separator.snp.updateConstraints{make in
+            make.height.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview()
+            make.left.equalTo(separator.snp.right).offset(-3)
+        }*/
+    }
 }
