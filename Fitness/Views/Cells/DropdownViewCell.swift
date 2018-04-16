ce8da1287b95d63f0976c22c565616214cdd8e67
Joseph Fulgieri <jmf373@cornell.edu>
2018-04-15 23:02:40 -0400

    replaced custom dropdowns with tableviews

--- Fitness/Views/Cells/DropdownViewCell.swift
+++ Fitness/Views/Cells/DropdownViewCell.swift
@@ -1,24 +1,27 @@
 //
-//  FilterDropdownCell.swift
+//  DropdownViewCell.swift
 //  Fitness
 //
-//  Created by Joseph Fulgieri on 4/2/18.
+//  Created by Joseph Fulgieri on 4/15/18.
 //  Copyright © 2018 Keivan Shahida. All rights reserved.
 //
 
 import UIKit
 import SnapKit
 
-class FilterDropdownCell: UIView {
-
+class DropdownViewCell: UITableViewCell {
+    
     // MARK: - INITIALIZATION
     var titleLabel: UILabel!
     var checkBox: UIView!
     var checkBoxColoring: UIView!
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+        self.contentView.backgroundColor = .white
         
+        //TITLE LABEL
         titleLabel = UILabel()
         titleLabel.sizeToFit()
         titleLabel.font = ._14MontserratLight
@@ -26,6 +29,7 @@ class FilterDropdownCell: UIView {
         titleLabel.text = "ZUMBA"
         addSubview(titleLabel)
         
+        //CHECKBOX
         checkBox = UIView()
         checkBox.layer.cornerRadius = 3
         checkBox.layer.borderColor = UIColor.fitnessLightGrey.cgColor
@@ -39,6 +43,14 @@ class FilterDropdownCell: UIView {
         checkBoxColoring.backgroundColor = .fitnessYellow
         checkBoxColoring.layer.cornerRadius = 1
         checkBox.addSubview(checkBoxColoring)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
         
         setupConstraints()
     }
@@ -67,14 +79,8 @@ class FilterDropdownCell: UIView {
         }
         
         checkBoxColoring.snp.updateConstraints{make in
-            make.top.equalToSuperview().offset(3)
-            make.left.equalToSuperview().offset(3)
-            make.right.equalToSuperview().offset(-3)
-            make.bottom.equalToSuperview().offset(-3)
+            make.top.left.equalToSuperview().offset(3)
+            make.bottom.right.equalToSuperview().offset(-3)
         }
     }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }   
 }
---
+++ Fitness/Views/Headers/DropdownHeaderView.swift
@@ -0,0 +1,65 @@
+//
+//  DropdownHeaderView.swift
+//  Fitness
+//
+//  Created by Joseph Fulgieri on 4/15/18.
+//  Copyright © 2018 Keivan Shahida. All rights reserved.
+//
+
+import UIKit
+
+class DropdownHeaderView: UITableViewHeaderFooterView {
+
+    // MARK: - INITIALIZATION
+    var titleLabel: UILabel!
+    var rightArrow: UIImageView!
+    var downArrow: UIImageView!
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        layer.backgroundColor = UIColor.white.cgColor
+        
+        titleLabel = UILabel()
+        titleLabel.font = ._12LatoBlack
+        titleLabel.textColor = .fitnessDarkGrey
+        titleLabel.sizeToFit()
+        contentView.addSubview(titleLabel)
+        
+        rightArrow = UIImageView(image: #imageLiteral(resourceName: "right_arrow"))
+        contentView.addSubview(rightArrow)
+        
+        downArrow = UIImageView(image: .none )
+        contentView.addSubview(downArrow)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - CONSTRAINTS
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        setupConstraints()
+    }
+    
+    func setupConstraints(){
+        titleLabel.snp.updateConstraints{make in
+            make.left.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+        }
+        
+        rightArrow.snp.updateConstraints{make in
+            make.centerY.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(4)
+            make.right.equalToSuperview().offset(-31)
+            make.left.equalTo(rightArrow.snp.right).offset(-8)
+        }
+        
+        downArrow.snp.updateConstraints{make in
+            make.centerY.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(6)
+            make.right.equalToSuperview().offset(-29)
+            make.left.equalTo(downArrow.snp.right).offset(-12)
+        }
+    }
+}
