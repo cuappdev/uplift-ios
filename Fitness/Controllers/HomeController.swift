c520808b3891d0cb5e5df42361dacd56f8caf478
Joseph Fulgieri <jmf373@cornell.edu>
2018-08-29 21:06:22 -0400

    Refactored homescreen tableviews

--- Fitness/Controllers/HomeController.swift
+++ Fitness/Controllers/HomeController.swift
@@ -8,12 +8,19 @@
 import UIKit
 import SnapKit
 
+enum SectionType {
+    case allGyms
+    case todaysClasses
+    case lookingFor
+}
+
 class HomeController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // MARK: - INITIALIZATION
     var tableView: UITableView!
     var statusBarBackgroundColor: UIView!
     
+    var sections = [SectionType]()
     var gyms = [Gym]()
     var gymClassInstances = [GymClassInstance]()
     var gymLocations = [Int: String]()
@@ -35,6 +42,10 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.register(TodaysClassesCell.self, forCellReuseIdentifier: "todaysClassesCell")
         tableView.register(AllGymsCell.self, forCellReuseIdentifier: "allGymsCell")
         tableView.register(LookingForCell.self, forCellReuseIdentifier: "lookingForCell")
+        
+        sections.insert(.allGyms, at: 0)
+        sections.insert(.todaysClasses, at: 1)
+        sections.insert(.lookingFor, at: 2)
         
         view.addSubview(tableView)
         
@@ -79,84 +90,69 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
     // MARK: - TABLE VIEW
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        switch indexPath.section {
-        case 0:
+        switch sections[indexPath.section] {
+        case .allGyms:
             let cell = tableView.dequeueReusableCell(withIdentifier: "allGymsCell", for: indexPath) as! AllGymsCell
             cell.gyms = gyms
-            cell.navigationController = navigationController ?? nil
+            cell.navigationController = navigationController
             return cell
-        case 1:
+        case .todaysClasses:
             let cell = tableView.dequeueReusableCell(withIdentifier: "todaysClassesCell", for: indexPath) as! TodaysClassesCell
             cell.gymClassInstances = gymClassInstances
             cell.gymLocations = gymLocations
-            cell.navigationController = navigationController ?? nil
+            cell.navigationController = navigationController
             return cell
-        case 2:
+        case .lookingFor:
             let cell = tableView.dequeueReusableCell(withIdentifier: "lookingForCell", for: indexPath) as! LookingForCell
             cell.tags = tags
             return cell
-        default:
-            return UITableViewCell()
         }
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
-        switch section {
-        case 0:
+        switch sections[section] {
+        case .allGyms:
             let header = HomeScreenHeaderView(reuseIdentifier: "homeScreenHeaderView", name: "Joe")
             header.subHeader.titleLabel.text = "ALL GYMS"
             return header
-        case 1:
+        case .todaysClasses:
             let header = HomeSectionHeaderView(frame: view.frame)
             header.titleLabel.text = "TODAY'S CLASSES"
             return header
-        case 2:
+        case .lookingFor:
             let header = HomeSectionHeaderView(frame: view.frame)
             header.titleLabel.text = "I'M LOOKING FOR..."
             return header
-        default:
-            return UIView()
         }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 4
+        return sections.count
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        var height: CGFloat
         
-        switch section {
-        case 0:
-            height = 155
-        case 1:
-            height = 51
-        case 2:
-            height = 51
-        default:
-            height = 0
+        switch sections[section] {
+        case .allGyms:
+            return 155
+        case .todaysClasses:
+            return 51
+        case .lookingFor:
+            return 51
         }
-        
-        return height
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        var height: CGFloat
-        
-        switch indexPath.section {
-        case 0:
-            height = 180
-        case 1:
-            height = 207
-        case 2:
-            height = CGFloat(143 * (tags.count / 2))
-        default:
-            height = 0
+        switch sections[indexPath.section] {
+        case .allGyms:
+            return 180
+        case .todaysClasses:
+            return 207
+        case .lookingFor:
+            return CGFloat(143 * (tags.count / 2))
         }
-        
-        return height
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
