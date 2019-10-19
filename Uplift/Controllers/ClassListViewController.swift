569512bdb1ea956a18707515afd4efaf7d142cb9
Yana Sang <yws3@cornell.edu>
2019-10-19 17:50:08 -0400

    PR change

--- Uplift/Controllers/ClassListViewController.swift
+++ Uplift/Controllers/ClassListViewController.swift
@@ -152,9 +152,7 @@ class ClassListViewController: UIViewController {
             NetworkManager.shared.getGymClassesForDate(date: dateFormatter.string(from: date)) { [weak self] classes in
                 guard let strongSelf = self else { return }
 
-                strongSelf.classList[index] = classes.sorted { (first, second) in
-                    return first.startTime < second.startTime
-                }
+                strongSelf.classList[index] = classes.sorted(by: { $0.startTime < $1.startTime })
                 strongSelf.updateClassCollectionViewWithFilters()
             }
             return
