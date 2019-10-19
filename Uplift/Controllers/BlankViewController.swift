ae12c878799b0c2c6b3c7123457a00275d760a86
Phillip O'Reggio <pnoreggio@gmail.com>
2019-10-19 14:46:35 -0400

    start blank testing view

---
+++ Uplift/Controllers/BlankViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BlankViewController.swift
+//  Uplift
+//
+//  Created by Phillip OReggio on 10/19/19.
+//  Copyright © 2019 Cornell AppDev. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class BlankViewController: UIViewController {
+
+    var test: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .fitnessLightGrey
+        test = GymDetailWeekView()
+        view.addSubview(test)
+        setupConstraints()
+    }
+
+    func setupConstraints() {
+        test.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview() 
+            make.center.equalToSuperview()
+            make.height.equalTo(70)
+        }
+    }
+}
