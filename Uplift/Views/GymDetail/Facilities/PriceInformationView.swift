0e4f801de18aa62839cace33fd6475fd52dc6630
Yana Sang <yws3@cornell.edu>
2019-10-19 15:43:29 -0400

    Set up views

--- Uplift/Views/GymDetail/Facilities/PriceInformationView.swift
+++ Uplift/Views/GymDetail/Facilities/PriceInformationView.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class PriceInformationView: UIView {
-    // MARK: - Private view vars
+
+    /// MARK: - Private view vars
     private let backgroundView: UIView = UIView()
     private let costsList: UITextView = UITextView()
     private let itemsList: UITextView = UITextView()
+
+    /// MARK: - Private layout vars
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,11 +23,34 @@ class PriceInformationView: UIView {
     
     private func setupViews() {
         backgroundView.layer.cornerRadius = 4.0
-        backgroundView.backgroundColor = 
+        // replace with gray01 once yana/colors is merged
+        backgroundView.backgroundColor = UIColor.colorFromCode(0xE5ECED)
+        addSubview(backgroundView)
     }
 
     private func setupConstraints() {
-        
+        let backgroundHeight: CGFloat = 202
+        let backgroundWidth: CGFloat = 68
+        let contentPadding: CGFloat = 12
+        let costsListWidth: CGFloat = 39
+
+        backgroundView.snp.makeConstraints{ make in
+            make.center.equalToSuperview()
+            make.width.equalTo(backgroundWidth)
+            make.height.equalTo(backgroundHeight)
+        }
+
+        costsList.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(contentPadding)
+            make.trailing.bottom.equalToSuperview().inset(contentPadding)
+            make.width.equalTo(costsListWidth)
+        }
+
+        itemsList.snp.makeConstraints{ make in
+            make.top.leading.equalToSuperview().offset(contentPadding)
+            make.trailing.equalTo(costsList.snp.leading).offset(-contentPadding)
+            make.bottom.equalToSuperview().inset(contentPadding)
+        }
     }
 
     required init?(coder: NSCoder) {
