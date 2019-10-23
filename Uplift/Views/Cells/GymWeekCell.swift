be2cd349f5967f759d3dcefa901c7db5a0d40f05
Phillip O'Reggio <pnoreggio@gmail.com>
2019-10-23 00:12:42 -0400

    fix color in gymWeekView

--- Uplift/Views/Cells/GymWeekCell.swift
+++ Uplift/Views/Cells/GymWeekCell.swift
@@ -72,6 +72,6 @@ class GymWeekCell: UICollectionViewCell {
     /// Updates the appearence of the cell when selection changes
     func update() {
         backCircle.alpha = isSelected || today ? 1 : 0
-        backCircle.backgroundColor = isSelected ? .primaryYellow : .gray02
+        backCircle.backgroundColor = isSelected ? .primaryYellow : .gray01
     }
 }
