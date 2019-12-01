722a9d4297e09f88cecedf9c89e2ec88482b5948
Phillip O'Reggio <pnoreggio@gmail.com>
2019-12-01 12:38:42 -0500

    Create untested networking code

--- Uplift/AppDelegate.swift
+++ Uplift/AppDelegate.swift
@@ -58,13 +58,69 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Onboarding
     private func setupOnboardingViewController() -> OnboardingViewController {
-        let gyms: [String] = ["Helen Newman", "Appel", "Teagle", "Noyes"]
-        let classes: [GymClassInstance] = [
-                GymClassInstance(classDescription: "", classDetailId: ClassIds.yogaVinyasa, className: "Yoga Vinyasa", duration: 0, endTime: Date(), gymId: "", imageURL: URL(fileURLWithPath: ""), instructor: "", isCancelled: false, location: "", startTime: Date(), tags: []),
-                GymClassInstance(classDescription: "", classDetailId: ClassIds.CURowShockwave, className: "CU Row (Shockwave)", duration: 0, endTime: Date(), gymId: "", imageURL: URL(fileURLWithPath: ""), instructor: "", isCancelled: false, location: "", startTime: Date(), tags: []),
-                GymClassInstance(classDescription: "", classDetailId: ClassIds.zumba, className: "Zumba", duration: 0, endTime: Date(), gymId: "", imageURL: URL(fileURLWithPath: ""), instructor: "", isCancelled: false, location: "", startTime: Date(), tags: []),
-                GymClassInstance(classDescription: "", classDetailId: ClassIds.musclePump, className: "Muscle Pump", duration: 0, endTime: Date(), gymId: "", imageURL: URL(fileURLWithPath: ""), instructor: "", isCancelled: false, location: "", startTime: Date(), tags: [])
-            ]
+        var gyms: [String] = []
+        var classes: [GymClassInstance] = []
+
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
+        dispatchGroup.enter()
+
+        NetworkManager.shared.getGymNames {
+            gyms = $0.map { $0.name }
+            // Merge Teagle Up and Down
+            if let teagleUp = gyms.firstIndex(of: "Teagle Up"), let teagleDown = gyms.firstIndex(of: "Teagle Down") {
+                gyms.remove(at: teagleUp)
+                gyms.remove(at: teagleDown)
+                gyms.append("Teagle")
+            }
+            dispatchGroup.leave()
+        }
+
+        let dateFormatter = DateFormatter()
+        // Display 4 classes from different (random) tag categories
+        NetworkManager.shared.getGymClassesForDate(date: dateFormatter.string(from: Date())) { classInstaces in
+            // Less than 4 classes -> Use Hard coded defaults
+            if classInstaces.count < 4 {
+                dispatchGroup.leave()
+                return
+            }
+
+            NetworkManager.shared.getTags { tags in
+                // Sample 4 without replacement
+                var randomIndeces = Set<Int>()
+                while randomIndeces.count < 4 {
+                    let index = Int.random(in: 0..<tags.count)
+                    randomIndeces.insert(index)
+                }
+                // Tag categories not yet chosen to be displayed
+                var tagSubset = randomIndeces.map { tags[$0] }
+
+                // Iterate over ique classes that don't share the same name
+                for instance in classInstaces {
+                    if !classes.contains(where: { $0.className == instance.className }) {
+                        // Add class only if it has a tag not yet displayed
+                        for tag in tagSubset {
+                            if let index = tagSubset.index(of: $0) {
+                                // Remove chosen class's tag from subset, add to tags to be displayed
+                                tagSubset.remove(at: index)
+                                classes.append(instance)
+                                break
+                            }
+                        }
+                        // Chose 4 classes, can stop
+                        if classes.count >= 4 { break }
+                    }
+                }
+
+                dispatchGroup.leave()
+            }
+        }
+
+        DispatchGroup.notify(queue: .main, execute: {
+            self.window?.rootViewController = classes.count < 4
+                ? OnboardingViewController()
+                : OnboardingViewController(gymNames: gyms, classes: classes)
+        })
 
        return OnboardingViewController(gymNames: gyms, classes: classes)
     }
