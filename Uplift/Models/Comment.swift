a6ec0a6cea273e3b818dd1a75df2cc9d166d0641
Cameron Hamidi <cah376@cornell.edu>
2020-02-12 11:14:01 -0500

    Finish Post and Comment models

---
+++ Uplift/Models/Comment.swift
@@ -0,0 +1,19 @@
+//
+//  Comment.swift
+//  Uplift
+//
+//  Created by Cameron Hamidi on 2/12/20.
+//  Copyright © 2020 Cornell AppDev. All rights reserved.
+//
+
+import Foundation
+
+struct Comment: Codable {
+
+    let createdAt: Date!
+    let id: Int!
+    let postId: Int!
+    let text: String!
+    let userId: Int!
+
+}
---
+++ Uplift/Views/LoadingScreenHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingScreenHomeView.swift
+//  Alamofire
+//
+//  Created by Cameron Hamidi on 2/7/20.
+//
+
+import UIKit
+import SnapKit
+
+class LoadingScreenHomeView: UIView {
+
+    let navBarGreyedOut = UIView()
+    let tabBarGreyedOut = UIView()
+    
+    
+
+}
