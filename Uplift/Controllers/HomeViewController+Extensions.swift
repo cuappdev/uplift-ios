a4d0f5ae01acb5c5c917cae8de4b81f1a945fb00
Elvis Marcelo <75585259+emarc0314@users.noreply.github.com>
2023-03-02 14:14:17 -0500

    Update HomeViewController+Extensions.swift

--- Uplift/Controllers/HomeViewController+Extensions.swift
+++ Uplift/Controllers/HomeViewController+Extensions.swift
@@ -11,7 +11,6 @@ import UIKit
 extension HomeViewController: UICollectionViewDataSource {
 
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        print("these are the sections \(sections.count)")
         return sections.count
     }
 
