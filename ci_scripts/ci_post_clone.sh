a4b39cc59f9923f4670f07096e1bd44af06eb9a0
Tiffany <tp284@cornell.edu>
2023-10-25 18:57:00 -0400

    Added initial script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Uplift
+#
+#  Created by Tiffany Pan on 10/25/23.
+#  Copyright © 2023 Cornell AppDev. All rights reserved.
+
+echo "Installing Cocoapods..."
+brew install cocoapods
+pod deintegrate
+pod install
+
+echo "Installing Secrets..."
+brew install wget
+wget -O ../Uplift/GoogleService-Info.plist "$GOOGLE_SERVICE_PLIST"
+wget -O ../Uplift/Keys.plist "$KEYS_PLIST"
