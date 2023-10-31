01af1c039c5af6fa3c104c5afb8237287c8a8edd
Tiffany <tp284@cornell.edu>
2023-10-31 09:52:17 -0400

    Added GraphQL generation

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -22,3 +22,13 @@ cd Secrets
 touch Keys.plist
 
 wget -O ../Secrets/Keys.plist "$KEYS_PLIST"
+
+# Path to your .graphql files
+GRAPHQL_FILES_PATH="../Uplift/graphql"
+
+# Output directory for generated Swift files
+OUTPUT_DIRECTORY="../Uplift/"
+
+apollo schema:download --endpoint=$PROD_ENDPOINT ../Uplift/graphql/schema.json
+apollo codegen:generate --localSchemaFile="$GRAPHQL_FILES_PATH/schema.json" --target=swift --includes="$GRAPHQL_FILES_PATH/*.graphql" --output="$OUTPUT_DIRECTORY"
+
